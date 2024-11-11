1783c2574bc13378207edfc9e2c6d622f7a0b6b2
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-11-11 14:40:14 -0300

    clean cnpj before send to database

--- employee/cadastrarF.php
+++ employee/cadastrarF.php
@@ -13,9 +13,10 @@ $termination_date = $_POST['termination_date'];
 $company_cnpj = $_POST['company_cnpj'];
 
 $clean_cpf = preg_replace('/[^0-9]/', '', $person_cpf);
+$clean_cnpj = preg_replace('/[^0-9]/', '', $company_cnpj);
 
 $stmt = $conn->prepare("INSERT INTO employees (person_cpf, department, role, salary, assignment, contract_type, hire_date, termination_date, company_cnpj) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-$stmt->execute([$clean_cpf, $department, $role, $salary, $assignment, $contract_type, $hire_date, $termination_date, $company_cnpj]);
+$stmt->execute([$clean_cpf, $department, $role, $salary, $assignment, $contract_type, $hire_date, $termination_date, $clean_cnpj]);
 
 echo "Funcionário cadastrado com sucesso";
 $conn = null;
